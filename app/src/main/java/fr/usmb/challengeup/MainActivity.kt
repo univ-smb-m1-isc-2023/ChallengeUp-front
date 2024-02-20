af8293fc5e24bdf82919c5195161dccd4c0fe6f5
JulienGln <juliengalerne.jg@gmail.com>
2024-02-20 11:15:48 +0100

    Switch vers Material3 + page connexion en cours

--- app/src/main/java/fr/usmb/challengeup/MainActivity.kt
+++ app/src/main/java/fr/usmb/challengeup/MainActivity.kt
@@ -2,10 +2,21 @@ package fr.usmb.challengeup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "Connexion"
+
+        val loginButton =findViewById<Button>(R.id.login)
+
+        loginButton.setOnClickListener {
+            Snackbar
+                .make(loginButton, "Vous avez cliquer sur \"Se connecter\"", Snackbar.LENGTH_SHORT)
+                .show()
+        }
     }
 }
