b029d8d90ca8fa94bd4a57376f7b05318a29140f
JulienGln <juliengalerne.jg@gmail.com>
2024-03-02 15:08:01 +0100

    Suite newChallengeActivity
    Co-Authored-By: TheoCmb <98331066+TheoCmb@users.noreply.github.com>

--- app/src/main/java/fr/usmb/challengeup/NewChallengeActivity.kt
+++ app/src/main/java/fr/usmb/challengeup/NewChallengeActivity.kt
@@ -2,11 +2,18 @@ package fr.usmb.challengeup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AutoCompleteTextView
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.google.android.material.textfield.TextInputLayout
 
 class NewChallengeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_challenge)
         supportActionBar?.hide()
+
+        //val tagTextView = findViewById<TextInputLayout>(R.id.newChallengeTag)
+        val tags = arrayOf("Sport", "Culture", "Cuisine")
+        //(tagTextView.editText as? MaterialAutoCompleteTextView)?.setSimpleItems(tags)
     }
 }
